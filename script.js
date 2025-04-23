ff0805550fa175fc41b4cce046b49d3678abaa40
MoizRed <abdoureduniv2000@gmail.com>
2025-04-23 15:34:21 +0100

    update excutablepath

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ app.use(morgan("dev"));
     const Govid = process.env.GOVNID
     const token = process.env.TOKEN
 //LAUNCH
-const browser = await puppeteer.launch({
+const browser = await new puppeteer.launch({
     headless : true,
     args: ['--disable-setuid-sandbox' , '--no-sandbox' , '--single-process' , "--no-zygote",],
     executablePath : process.env.NODE_ENV === "production" ? process.env.PUPPETEER_EXECUTABLE_PATH  : puppeteer.executablePath()  ,
